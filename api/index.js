242dfd46a7e9185e35062dbdb6bc231fdbf2e20b
jawwad-19 <jawwad.ahmadd19@gmail.com>
2026-04-19 03:09:28 -0700

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+console.log('MONGO env var:', process.env.MONGO);
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('Successfully Connected to MongoDB !'))
+  .catch(err => console.log(err));
 const app=express();
 app.listen(5000,()=>{
     console.log('Server is running on port 5000!!');
